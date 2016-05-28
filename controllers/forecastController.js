6cf5688322c0117f212e9e86db215f72c38f5459
juanocasio <juanocasio@comcast.net>
2016-05-28 17:00:35 -0400

    added weather api

--- controllers/forecastController.js
+++ controllers/forecastController.js
@@ -1,5 +1,29 @@
 'use strict';
 weatherForecast
-    .controller('forecastController', ['$scope', '$routeParams','forecastService', function ($scope, $routeParam, forecastService) {
+    .controller('forecastController', ['$scope', '$resource', 'forecastService', function ($scope, $resource, forecastService) {
         $scope.city = forecastService.city;
+
+        $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily?APPID=1e6d8bcb816fdcd89655f4c3c4b5c217',
+            {
+                callback: "JSON_CALLBACK"
+            },
+            {
+                get: {
+                    method: "JSONP"
+                }
+            });
+            
+            $scope.weatherResults = $scope.weatherAPI.get(
+                {
+                    q: $scope.city,
+                    cnt: 2
+                });
+                
+            $scope.convertToFahrenheit = function ( degK ) {
+                return Math.round((1.8 * (degK - 273)) + 32);
+            }
+            
+            $scope.convertToDate = function (dt){
+                return new Date(dt * 1000);
+            }
     }]);
